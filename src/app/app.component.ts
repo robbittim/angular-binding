f6c38f4318593d0d4a6c142c48e660e8cd4b373f
tim <robbittim@gmail.com>
2022-05-25 15:35:11 -0500

    add directive example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-binding';
   username = '';
+
+  showSecret = false;
+  // log: number[] = [];
+  log: Date[] = [];
+
+  onToggleDisplay() {
+    this.showSecret = !this.showSecret;
+
+    // this.log.push(this.log.length + 1);
+
+    this.log.push(new Date());
+  }
 }
